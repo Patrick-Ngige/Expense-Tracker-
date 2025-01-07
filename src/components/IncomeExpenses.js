39491db2be1312d917dc93aac05e88f624209c77
Patrick-Ngige <wakemanjajr@gmail.com>
2025-01-07 22:26:04 +0300

    updating

---
+++ src/components/IncomeExpenses.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const IncomeExpenses = () => {
+  return (
+    <div>
+      <h4>Income</h4>
+      <p id='money-plus' className="money plus">+$0.00</p>
+    </div>
+  )
+}
+
