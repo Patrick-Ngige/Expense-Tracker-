a4cc3a0417d8562434b8c15be4bc050a95b7bc50
Patrick-Ngige <wakemanjajr@gmail.com>
2025-01-07 23:06:03 +0300

    doing away with IDs

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 const AddTransaction = () => {
+
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+
   return (
     <>
         <h3>Add new Transaction</h3>
         <form>
-            <div class="form-control">
+            <div className="form-control">
                 <label htmlFor="text">Text</label>
-                <input type="text" placeholder="Enter text..." />
+                <input type="text" value={text} onChange={(e) =>setText(e.target.value)} placeholder="Enter text..." />
             </div> 
             <div className='form-control'>
                 <label htmlFor="amount">Amount <br/>
                 (negative - expense, positive - income)</label>
-                <input type='number' placeholder='Enter the Amount ...'></input>
+                <input type='number' value={amount} onChange={(e) =>setAmount(e.target.value)} placeholder='Enter the Amount ...'></input>
             </div>
             <button className="btn">Add transaction</button>
         </form>
