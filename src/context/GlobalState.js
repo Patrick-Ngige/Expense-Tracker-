1a3af894143ab97230631b055faf02020a164e2e
Patrick-Ngige <wakemanjajr@gmail.com>
2025-01-08 22:09:32 +0300

    creating the global state

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -9,3 +9,19 @@ const initialState = {
         { id: 4, text: 'Camera', amount: 150 },
     ]
 }
+
+// create context
+export const GlobalContext = createContext(initialState);
+
+// provider component
+export const GlobalProvider  ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return ( <GlobalContext.Provider value={{
+        transactions: state.transactions
+    }}> 
+
+    {children}
+
+    </GlobalContext.Provider>)
+}
