3993001b49ed517e0c2fd3c27d025a696ea24ce6
Patrick-Ngige <wakemanjajr@gmail.com>
2025-01-08 23:55:01 +0300

    making the delete button functional

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,6 +1,11 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
     switch(action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transaction.filter(transaction => transaction.id !== action.payload)
+            }
         default:
             return state;
     }
